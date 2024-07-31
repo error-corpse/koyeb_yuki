975797981ce5646d01a6e9e30890e594e7d61b65
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-07-31 16:17:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ from pyrogram import filters
 load_dotenv()
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002092954715"))
+
+
+PORT = os.environ.get("PORT", "8000")
